0e79c435c465a8e72a773521a611984b39dae742
Cole Ferguson <coleferguson9@gmail.com>
2020-11-10 16:58:01 -0800

    Added a gitignore for pycharm dirs, as well as assignment files

---
+++ driver.py
@@ -0,0 +1,27 @@
+"""
+Machine learning
+decision trees
+"""
+import time
+
+
+from ml_lib.ml_util import DataSet
+
+from decision_tree import  DecisionTreeLearner
+
+from ml_lib.crossval import cross_validation
+
+from statistics import mean, stdev
+    
+
+
+    
+def main():
+    """
+    Machine learning with decision trees.
+    Runs cross validation on data sets and reports results/trees
+    """
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    main()
