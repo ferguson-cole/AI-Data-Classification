5f107d1bebcfc785f9ad83ca2f91db924a13db45
Cole Ferguson <coleferguson9@gmail.com>
2020-11-17 23:05:38 -0800

    final commit as of 11/17 (removed prints, added affidavit)

--- driver.py
+++ driver.py
@@ -1,6 +1,15 @@
 """
-Machine learning
-decision trees
+ We the undersigned promise that we have in good faith attempted to follow the principles of pair programming. 
+ Although we were free to discuss ideas with others, the implementation is our own. We have shared a common workspace 
+ and taken turns at the keyboard for the majority of the work that we are submitting. Furthermore, any non programming 
+ portions of the assignment were done independently. We recognize that should this not be the case, we will be subject 
+ to penalties as outlined in the course syllabus.
+
+Pair Programmer 1 (print & sign your name, then date it)
+Cole Ferguson, 11/17/2020
+
+Pair Programmer 2 (print & sign your name, then date it) 
+Ryan Hildebrant, 11/17/2020
 """
 import time
 
